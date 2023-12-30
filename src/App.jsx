31d3ec591872e00c64543315d007885fbb76f497
Ankit <ankit26kushwa@gmail.com>
2023-12-30 15:22:06 +0530

    Set routes for Login and Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css';
-import Login from './components/Login';
+import "./App.css";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = useSelector((state) => state.login);
+  console.log(user.data.length);
+
+  const isloggedIn = () => {
+    if (user.data.length === 0) {
+      return <Route path="/" element={<Login />} />;
+    } else {
+      return <Route path="/" element={<Home />} />;
+    }
+  };
+
   return (
-    <>
-      <Login />
-    </>
+    <BrowserRouter>
+      <Routes>{isloggedIn()}</Routes>
+    </BrowserRouter>
   );
 }
 
