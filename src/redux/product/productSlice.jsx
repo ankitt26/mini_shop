4d3b91e5809d746a5fbc648593d76d032afae27f
Ankit <ankit26kushwa@gmail.com>
2023-12-31 13:11:45 +0530

    Create ProductSlice

---
+++ src/redux/product/productSlice.jsx
@@ -0,0 +1,50 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const FetchProducts = createAsyncThunk("fetch products", async () => {
+  const url = "https://dummyjson.com/products/";
+  try {
+    const response = await axios.get(url);
+    const data = response.data.products;
+    return data;
+  } catch (error) {
+    console.error("Something went wrong! :", error);
+  }
+});
+
+const ProductSlice = createSlice({
+  name: "products",
+  initialState: {
+    products: [],
+    cart: 0,
+    isLoading: false,
+    error: null,
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      state.cart += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(FetchProducts.pending, (state) => ({
+      ...state,
+      isLoading: true,
+      error: undefined,
+    }));
+    builder.addCase(FetchProducts.rejected, (state) => ({
+      ...state,
+      isLoading: false,
+      error: true,
+    }));
+    builder.addCase(FetchProducts.fulfilled, (state, action) => ({
+      ...state,
+      products: action.payload,
+      isLoading: false,
+      error: undefined,
+    }));
+  },
+});
+
+export default ProductSlice.reducer;
+export { FetchProducts };
+export const { addToCart } = ProductSlice.actions;
